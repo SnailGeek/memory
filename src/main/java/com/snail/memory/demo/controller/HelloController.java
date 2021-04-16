0edc9cfacef61f6287a3cf659832863adc1eaaaa
3E-6-2-80  王帆(10023366) <wangf-q@glodon.com>
2021-04-16 08:46:07 +0800

    去除导入无用的包

--- src/main/java/com/snail/memory/demo/controller/HelloController.java
+++ src/main/java/com/snail/memory/demo/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.snail.memory.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
