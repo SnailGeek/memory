f79c75ad42016d714dad5d2e10e1ad2fcbbe01e4
3E-6-2-80  王帆(10023366) <wangf-q@glodon.com>
2021-04-16 09:47:04 +0800

    客户端服务发现

--- src/main/java/com/snail/memory/demo/controller/HelloController.java
+++ src/main/java/com/snail/memory/demo/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.snail.memory.demo.controller;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: wangf-q
  * @date: 2021-04-15 17:32
  **/
+@Slf4j
 @RestController
 public class HelloController {
 
+    @Autowired
+    private DiscoveryClient client;
+
     @RequestMapping("/hello")
+
     public String index(@RequestParam("name") String name) {
+        client.getServices().forEach(serviceId -> client.getInstances(serviceId).forEach(serviceInstance ->
+                log.info("/hello, host:" + serviceInstance.getHost() + ", service_id:" + serviceInstance.getServiceId())));
         return "Hello " + name + "!";
     }
 }
--- src/test/java/com/snail/memory/demo/controller/HelloControllerTest.java
+++ src/test/java/com/snail/memory/demo/controller/HelloControllerTest.java
@@ -1,12 +1,10 @@
 package com.snail.memory.demo.controller;
 
 import com.snail.memory.MemoryApplication;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,14 +14,13 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = MemoryApplication.class)
 @WebAppConfiguration
 public class HelloControllerTest {
 
     private MockMvc mvc;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
     }
