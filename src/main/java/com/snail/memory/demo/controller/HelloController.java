74b7eb332d6ad5b4378528162aca2d1809e8cdb3
3E-6-2-80  王帆(10023366) <wangf-q@glodon.com>
2021-04-17 11:03:33 +0800

    修改demo接口

--- src/main/java/com/snail/memory/demo/controller/HelloController.java
+++ src/main/java/com/snail/memory/demo/controller/HelloController.java
@@ -3,7 +3,9 @@ package com.snail.memory.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,11 +23,13 @@ public class HelloController {
     @Autowired
     private DiscoveryClient client;
 
-    @RequestMapping("/hello")
-
-    public String index(@RequestParam("name") String name) {
-        client.getServices().forEach(serviceId -> client.getInstances(serviceId).forEach(serviceInstance ->
-                log.info("/hello, host:" + serviceInstance.getHost() + ", service_id:" + serviceInstance.getServiceId())));
-        return "Hello " + name + "!";
+    @GetMapping("/hello")
+    public String index() {
+        for (String serviceId : client.getServices()) {
+            for (ServiceInstance serviceInstance : client.getInstances(serviceId)) {
+                log.info("/hello, host:" + serviceInstance.getHost() + ", service_id:" + serviceInstance.getServiceId());
+            }
+        }
+        return "Hello ";
     }
 }
